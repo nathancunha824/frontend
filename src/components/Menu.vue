a547a1eb49116ed9abe9ab99b2ebc498af5c38f6
nathandacunha824 <nathandacunha.gamer@gmail.com>
2025-06-04 20:15:18 -0300

    ADD - Mudança do background-color das opções no sidebar,  barra de pesquisa junto com o icon

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -9,30 +9,49 @@
     <div id="title">
       <h1 class="fs-4 m-0">Agenda de contatos</h1>
     </div>
+    <div class="search">
+      <i class="bi bi-search" id = "iconSearch"></i><input type="search" name="" id="searchBar" placeholder="Pesquisar..">
+    </div>
   </div>
 
   <!-- Offcanvas -->
   <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
     <div class="offcanvas-header">
-      <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Backdrop with scrolling</h5>
+      <h1 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Painel</h1>
       <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body">
-      <p>Try scrolling the rest of the page to see this option in action.</p>
+      <h3>Contatos</h3>
+      <h3>Contatos frequentes</h3>
+      <h3>Configurações</h3>
     </div>
   </div>
 </template>
 
 <style scoped>
     #menuPrincipal {
-        background-color: #000080;
+      background-color: #000080;
+      font-family: "Ancizar Sans", sans-serif;
     }
     #title h1{
-        color: white;
-        font-family: "Ancizar Sans", sans-serif;
+      color: white;
+      font-family: "Ancizar Sans", sans-serif;
     }
     button {
-        font-size: 25px;
-        color: white;
+      font-size: 25px;
+      color: white;
+      font-family: "Ancizar Sans", sans-serif;
+    }
+    .offcanvas-body {
+      background-color: #f6f3f3;
+    }
+    #searchBar {
+      border-radius: 12px;
+      width: 332px;
+      margin-left: 15px;
+    }
+    #iconSearch {
+      color: white;
+      margin-left: 45px;
     }
 </style>
