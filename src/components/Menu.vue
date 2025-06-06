9ae49844de5a744e2e72f756af9b9c471d9c8804
nathandacunha824 <nathandacunha.gamer@gmail.com>
2025-06-06 19:17:33 -0300

    ADD - Adicionados os ícones de perfil e configurações

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,16 +1,25 @@
 <template>
-  <div class="d-flex align-items-center gap-3 px-3 py-2" id = "menuPrincipal">
+  <div class="d-flex align-items-center gap-3 px-3 py-2 w-100" id="menuPrincipal">
     <!-- Botão -->
     <button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">
       <i class="bi bi-list"></i>
     </button>
 
-    <!-- Título à direita -->
+    <!-- Título -->
     <div id="title">
       <h1 class="fs-4 m-0">Agenda de contatos</h1>
     </div>
-    <div class="search">
-      <i class="bi bi-search" id = "iconSearch"></i><input type="search" name="" id="searchBar" placeholder="Pesquisar..">
+
+    <!-- Pesquisa -->
+    <div class="search d-flex align-items-center">
+      <i class="bi bi-search" id="iconSearch"></i>
+      <input type="search" id="searchBar" placeholder="Pesquisar..">
+    </div>
+
+    <!-- Icons perfil/settings no canto direito -->
+    <div class="d-flex align-items-center gap-3 ms-auto" id="iconsHeader">
+      <i class="bi bi-person-circle" id="iconPerfil"></i>
+      <i class="bi bi-gear" id="iconSettings"></i>
     </div>
   </div>
 
@@ -25,33 +34,39 @@
       <h3>Contatos frequentes</h3>
       <h3>Configurações</h3>
     </div>
-  </div>
+  </div>  
 </template>
 
 <style scoped>
-    #menuPrincipal {
-      background-color: #000080;
-      font-family: "Ancizar Sans", sans-serif;
-    }
-    #title h1{
-      color: white;
-      font-family: "Ancizar Sans", sans-serif;
-    }
-    button {
-      font-size: 25px;
-      color: white;
-      font-family: "Ancizar Sans", sans-serif;
-    }
-    .offcanvas-body {
-      background-color: #f6f3f3;
-    }
-    #searchBar {
-      border-radius: 12px;
-      width: 332px;
-      margin-left: 15px;
-    }
-    #iconSearch {
-      color: white;
-      margin-left: 45px;
-    }
+  #menuPrincipal {
+    background-color: #000080;
+    font-family: "Ancizar Sans", sans-serif;
+  }
+  #title h1{
+    color: white;
+    font-family: "Ancizar Sans", sans-serif;
+  }
+  button {
+    font-size: 25px;
+    color: white;
+    font-family: "Ancizar Sans", sans-serif;
+  }
+  .offcanvas-body {
+    background-color: #f6f3f3;
+  }
+  #searchBar {
+    border-radius: 12px;
+    width: 332px;
+    margin-left: 15px;
+  }
+  #iconSearch {
+    color: white;
+    margin-left: 45px;
+  }
+
+  #iconsHeader, #iconPerfil i{
+    color: white;
+    font-size: 22px;
+    cursor: pointer;
+  }
 </style>
